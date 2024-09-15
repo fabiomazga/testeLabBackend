80a6702af87723e852e8b92d04e4f5db2cece651
fabiomazga <1323331031@uezo.edu.br>
2024-09-15 09:36:30 -0300

    Deploy inicial

--- src/main/java/com/testlab/AnamnesesServlet.java
+++ src/main/java/com/testlab/AnamnesesServlet.java
@@ -1,14 +1,35 @@
 package com.testlab;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
 import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
+@WebServlet("/anamneses")
 public class AnamnesesServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/json");
-        resp.getWriter().write(
-                "[{\"nome\":\"ANONIMO 1\", \"modalidade\":\"MR\", \"laudo\":\"F\", \"patientID\":\"ANONIMO\", \"data\":\"2020-06-09\", \"tipoExame\":\"SHOULDER\", \"numero\":\"780153-158\", \"estado\":\"4\"}]");
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        // Exemplo de dados JSON, que pode ser carregado de uma fonte de dados real
+        // posteriormente.
+        String jsonResponse = "[{"
+                + "\"nome\":\"ANONIMO 1\","
+                + "\"numAcesso\":\"780153-158\","
+                + "\"data\":\"2020-06-09\","
+                + "\"modalidade\":\"MR\","
+                + "\"tipoExame\":\"SHOULDER\","
+                + "\"estado\":\"4\""
+                + "}]";
+
+        PrintWriter out = response.getWriter();
+        out.print(jsonResponse);
+        out.flush();
     }
 }
